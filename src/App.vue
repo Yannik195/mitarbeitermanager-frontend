dbca0488257f2bbc8478155071819fdcbe991348
Yannik195 <yannisim@gmail.com>
2021-08-15 21:30:51 +0200

    bf axios

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2>MitarbeiterManager</h2>
       </div>
 
       <v-spacer></v-spacer>
@@ -54,3 +38,6 @@ export default {
   }),
 };
 </script>
+
+<style>
+</style>
