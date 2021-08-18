98fa1ce6296265be3b92d39c4ea08c9d78c97a22
Yannik195 <yannisim@gmail.com>
2021-08-18 19:00:57 +0200

    load data from api into employeeoverview site

--- src/sites/EmployeeOverview.vue
+++ src/sites/EmployeeOverview.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>This is the Overview</h1>
+    {{ response.data }}
 
     <router-link to="/add">
       <v-btn elevation="2" fab>
@@ -11,8 +12,24 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "EmployeeOverview",
+  data() {
+    return {
+      name: "Yannik",
+      lastname: "Simon",
+      response: null,
+    };
+  },
+  computed: {},
+  watch: {},
+  mounted() {
+    axios
+      .get("http://localhost:3000/employees/")
+      .then((response) => (this.response = response));
+  },
 };
 </script>
 
